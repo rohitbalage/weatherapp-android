743a353e8873553f320b19b2ab8981cde1f16778
Rohit Balage <thatcomputerboy26@gmail.com>
2022-12-21 19:20:19 -0500

    deploy dependecies and user permission

--- app/src/main/java/com/rrbofficial/weatherapp/MainActivity.java
+++ app/src/main/java/com/rrbofficial/weatherapp/MainActivity.java
@@ -1,8 +1,19 @@
 package com.rrbofficial.weatherapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         private TextView city, temperature, weatherCondition, humidity, maxTemperature, mimTemperature, pressure, wind;
         private ImageView imageView;
         private FloatingActionButton fab;
+
+        LocationManager locationManager;
+        LocationListener locationListener;
+        double lat, lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +48,40 @@ public class MainActivity extends AppCompatActivity {
            @Override
            public void onClick(View v) {
 
+
            }
        });
+       locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+       locationListener = new LocationListener() {
+           @Override
+           public void onLocationChanged(@NonNull Location location) {
+               lat = location.getLatitude();
+               lon = location.getLongitude();
+
+               Log.e("lat",String.valueOf(lat));
+               Log.e("lan",String.valueOf(lon));
+
+
+
+           }
+       };
+
+       if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION )!= PackageManager.PERMISSION_GRANTED){
+           ActivityCompat.requestPermissions (this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1 );
+
+        }
+       else{
+           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,500, 50,locationListener);
+       }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode ==1 && permissions.length > 0
+                && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION )== PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,500, 50,locationListener);
+        }
     }
 }
---
+++ app/src/main/java/com/rrbofficial/weatherapp/weatherAPI.java
@@ -0,0 +1,13 @@
+package com.rrbofficial.weatherapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface weatherAPI {
+    @GET ("/weather?appid=36ae13924525770df437914541fc1788")
+    Call<OpenWeatherMap>getWeatherWithLocation(@Query("lat" )double lat, @Query("lon")double lon);
+
+    @GET ("/weather?appid=36ae13924525770df437914541fc1788")
+    Call<OpenWeatherMap>getWeatherWithCityName(@Query("q" )String name);
+}
